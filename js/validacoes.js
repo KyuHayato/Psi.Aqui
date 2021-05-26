4a4b0d52cacb99eb685667f59d8665328f79484c
Ewerson Alves <ewerson.tre@gmail.com>
2021-05-26 15:46:00 -0300

    validações teste

--- js/validacoes.js
+++ js/validacoes.js
@@ -1,25 +1,47 @@
-function validaLogin(){
-    
-}
-function validaEmail(){
-    if(document.getElementsByName('email') == ''){
+
+/* function validaEmail(){
+    if(document.querySelector("#teste2").value == ''){
         alert("email obrigatorio")
     }
 }
 function validaSenha(){
-    if(document.getElementsByName('pass') == ''){
+    if(document.querySelector("#teste").value == ''){
         alert("senha obrigatoria")
+    }
+} */
+
+function validaLogin() {
+
+    if (document.querySelector("#teste2").value == '') {
+        alert('Insira um email valido, campo vazio')
+    }
+    if (document.querySelector("#teste").value == '') {
+        alert("senha obrigatoria")
+    }
+}
+function validaCadastro() {
+    if (document.querySelector('[data-nome]').value == "") {
+        alert("insira nome")
+    }
+    if (document.querySelector('[data-email]').value == "") {
+        alert("insira email")
+    }
+    if (document.querySelector("#genero").value == "valor0") {
+        alert("insira genero")
+    }
+    if(document.querySelector('[data-nasc]').value == ""){
+        alert("insira data de nasc")
+    
+    }
+    if(document.querySelector('[data-senha]').value == ""){
+        alert("insira senha valida")
+    
+    }
+    if(document.querySelector('[data-senha-conf]').value == ""){
+        alert("insira senha valida igual a primeira")
+    
     }
 }
 
-function valida(){
-    alert(document.querySelector("#teste").value)
-    if(document.querySelector("#teste").value == ''){
-        alert("email obrigatorio")
-        
-    }
-    alert(document.querySelector("#teste2").value)
-    if(document.querySelector("#teste2").value == ''){
-        alert("senha obrigatoria")
-    }
-}
+
+
